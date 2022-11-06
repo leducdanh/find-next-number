62e3180efed8a1245c549cef996f9b0d9e10b810
leducdanh <39113354+leducdanh@users.noreply.github.com>
2022-11-06 21:57:57 +0700

    refavtor User struct

--- golang/domain/entity/user/user.go
+++ golang/domain/entity/user/user.go
@@ -1,12 +1,13 @@
 package user
 
+import "time"
+
 type User struct {
-	ID       string `json:"id" db:"id" gorm:"primaryKey; column:id"`
-	Name     string `json:"name,omitempty"`
-	Logo     string `json:"logo,omitempty"`
-	Email    string `json:"email,omitempty"`
-	CretaeAt string `json:"create_at,omitempty"`
-	UpdateAt string `json:"update_at,omitempty"`
+	ID       string    `json:"id" db:"id" gorm:"primaryKey; column:id" validate:"required"`
+	Name     string    `json:"name,omitempty" validate:"required"`
+	Email    string    `json:"email,omitempty" validate:"required,email"`
+	CretaeAt time.Time `json:"create_at,omitempty"`
+	UpdateAt time.Time `json:"update_at,omitempty"`
 }
 
 func (User) TableName() string {
