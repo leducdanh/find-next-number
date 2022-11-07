2a13738fa3e8a7fe0c125a80842ddfd11a221418
leducdanh <39113354+leducdanh@users.noreply.github.com>
2022-11-07 21:59:16 +0700

    add middleware CORSWithConfig

--- golang/routes/main-routes.go
+++ golang/routes/main-routes.go
@@ -4,11 +4,16 @@ import (
 	"test-github/controller"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func New() *echo.Echo {
 	e := echo.New()
 	c := controller.Controller{}
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 
 	// create groups
 	apiGroup := e.Group("/api")
